d9dc92792d7ed6740d297ea70962b874eeb18ef8
Justin Z <megajustin@hotmail.com>
2014-01-08 18:45:45 +0800

    ignore UnknownHostException
    Parsed URLs (especially invalid ones) that cannot be reached should not print an error.
    
    [18:33:06] <justin_>	 http://like.this
    [18:33:07] <lordick>	 Error getting url data: java.net.UnknownHostException: like.this

--- bot/src/lordick/bot/commands/LinkMetadata.java
+++ bot/src/lordick/bot/commands/LinkMetadata.java
@@ -14,6 +14,7 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.UnknownHostException;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.sql.Statement;
@@ -141,6 +142,8 @@ public class LinkMetadata implements MessageListener, InitListener {
                 conn.setRequestProperty("Cookie", "mpr_agree4=yes");
             }
             parseConnection(result, conn);
+        } catch (UnknownHostException ignored) {
+            // Nope. Nobody really cares.
         } catch (Exception e) {
             result.append("Error getting url data: ").append(e);
         } finally {
@@ -176,7 +179,7 @@ public class LinkMetadata implements MessageListener, InitListener {
             }
             return;
         } else if (code >= 400) {
-            result.append("[SITE] Error: ").append(conn.getResponseMessage());
+            result.append("[SITE] : ").append(conn.getResponseMessage());
             return;
         }
         final CountDownLatch latch = new CountDownLatch(1);
